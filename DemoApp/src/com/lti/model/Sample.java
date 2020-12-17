77d7eaceb5fd9eaa8450372ee7f54bd4679143f2
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2020-12-17 16:58:51 +0530

    Added sample class

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,15 @@
+package com.lti.model;
+
+public class Sample {
+	
+	public void show()
+	{
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
