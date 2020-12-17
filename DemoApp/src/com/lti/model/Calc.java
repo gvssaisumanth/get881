efe2ecce222dd8a3b45217a3c9e796a8c70b5c80
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2020-12-17 17:17:45 +0530

    another branch

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Calc {
+
+	public void add(int n1,int n2)
+	{
+		System.out.println("Sum= "+ (n1+n2));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
