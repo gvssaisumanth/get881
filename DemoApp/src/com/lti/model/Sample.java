dc789fa35b42010fe8ad2d7eab6cefc6b7f06bc8
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2020-12-17 17:07:44 +0530

    added a method

--- DemoApp/src/com/lti/model/Sample.java
+++ DemoApp/src/com/lti/model/Sample.java
@@ -6,6 +6,11 @@ public class Sample {
 	{
 		System.out.println("Hello world");
 	}
+	
+	public void method()
+	{
+		System.out.println("naruhoodo....so yukotha...I see that's how it is");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
